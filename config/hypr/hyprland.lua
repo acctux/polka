8c38c4f290323c43f66f018678f37a7c72dd5d91
Nicholas Aghajanian <226946345+acctux@users.noreply.github.com>
2026-05-12 00:04:24 -0400

    p

---
+++ config/hypr/hyprland.lua
@@ -0,0 +1,11 @@
+-- This file sources other files in `hyprland` and `custom` folders
+-- You wanna add your stuff in files in `custom`
+
+-- Defaults --
+require("hyprland/general")
+require("hyprland/rules")
+require("hyprland/colors")
+require("hyprland/keybinds")
+-- nwg-displays support: re-add the files if it updates later
+-- require("workspaces")
+-- require("monitors")
---
+++ local/bin/keyboard/mute-input.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle >/dev/null
+if pactl get-source-mute @DEFAULT_SOURCE@ | rg -q 'yes'; then
+  led=on
+  osd_message='Microphone muted'
+  osd_icon='microphone-sensitivity-muted-symbolic'
+else
+  led=off
+  osd_message='Microphone on'
+  osd_icon='audio-input-microphone-symbolic'
+fi
+swayosd-client \
+  --custom-message "$osd_message" \
+  --custom-icon "$osd_icon"
