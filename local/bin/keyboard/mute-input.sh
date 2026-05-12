5065d44337fcacafea381dc7cecec9d0c870b670
Nicholas Aghajanian <226946345+acctux@users.noreply.github.com>
2026-05-12 00:11:15 -0400

    p

--- local/bin/keyboard/mute-input.sh
+++ local/bin/keyboard/mute-input.sh
@@ -2,11 +2,11 @@
 wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle >/dev/null
 if pactl get-source-mute @DEFAULT_SOURCE@ | rg -q 'yes'; then
   led=on
-  osd_message='Microphone muted'
+  osd_message='Mic muted'
   osd_icon='microphone-sensitivity-muted-symbolic'
 else
   led=off
-  osd_message='Microphone on'
+  osd_message='Mic on'
   osd_icon='audio-input-microphone-symbolic'
 fi
 swayosd-client \
